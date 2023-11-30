61e608dc40b7dc55bc882f9f62ad96b454203e71
Alexandre VARGAS LOPEZ <alexandre.vargas.lopez@gmail.com>
2023-11-30 13:55:29 +0100

    Tests helloworld sur flask

---
+++ app.py
@@ -0,0 +1,14 @@
+# /usr/bin/python3
+import views
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+app.add_url_rule('/', view_func=views.index)
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True)
+
+ 
+
