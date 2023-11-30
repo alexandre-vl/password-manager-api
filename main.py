0a63250f3bb41b8c5004a0dcacf40ef48c77c277
VARGAS LOPEZ Alexandre <alexandre.vargaslopez@lyc-degrasse-06.region-paca.lan>
2023-11-30 13:30:48 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# /usr/bin/python3
+
+import routes.index as rt
+
+class Main: 
+    def __init__(self):
+        self.test = 1
+        print('Hello word')
+
+
+    
+    
+
+main = Main()
+
+print(rt.hello_world)
---
+++ src/utils/chript.py
@@ -0,0 +1,8 @@
+
+
+class Chiffrement:
+    def __init__(self):
+        pass
+
+
+
