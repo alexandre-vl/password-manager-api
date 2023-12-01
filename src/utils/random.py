97fd6ef464f19e4394ab34c70c0aa277673f1a3c
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-12-01 22:18:33 +0100

    Refactor code and remove unused file

--- src/utils/random.py
+++ src/utils/random.py
@@ -1,35 +1,10 @@
 import time
 
 
-def now():
-    nowTime = time.time()
-    return int(nowTime * 1000)
+def random():
+    return int(str(time.time()).replace('.', '')[8:])
 
 
-def random(size: int):
-    numberListe = []
-    numberInt = ''
-    for i in range(size):
-        nowMiliTime = str(now())
-        numberListe.append(nowMiliTime[-1])
-        sleep = int(numberListe[i]) / 20
-        time.sleep(sleep)
-    for i in numberListe:
-        numberInt += i
-    return numberInt
+def randomRange(min, max):
+    return int(str(time.time()).replace('.', '')[8:]) % (max - min) + min
 
-
-def randint(start: int = 0, stop: int = 10):
-    size = start + stop
-    value = random(len(str(size)))
-    if int(value) > stop:
-        return random(len(str(start)))
-
-    elif int(value) < start:
-        return random(len(str(stop)))
-
-    else:
-        return int(value)
-
-
-# print(randint(start=0, stop=20))
