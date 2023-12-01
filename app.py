5bb81c2438e53d80115abe03c80e777bca243fb5
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2023-12-01 22:00:51 +0100

    Update app.py and index.html

--- app.py
+++ app.py
@@ -10,5 +10,3 @@ app.add_url_rule('/', view_func=views.index)
 if __name__ == '__main__':
     app.run(use_reloader=True)
 
- 
-
---
+++ pyside_test.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+import sys
+
+from PySide6.QtCore import Qt, QUrl
+from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel
+from PySide6.QtWebEngineWidgets import QWebEngineView
+
+CURRENT_DIRECTORY = Path(__file__).resolve().parent
+
+class webView(QWidget):
+    def __init__(self):
+        super(webView, self).__init__()
+
+        filename = os.fspath(CURRENT_DIRECTORY / "templates/index.html")
+        url = QUrl.fromLocalFile(filename)
+
+        self.webV = QWebEngineView()
+        self.webV.load(url)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.webV)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    web = webView()
+    web.show()
+
+    sys.exit(app.exec_())
