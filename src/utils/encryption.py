c4d8d38b5068b5ab5a9da57757da285915430ec5
Alexandre VARGAS LOPEZ <alexandre.vargas.lopez@gmail.com>
2023-12-18 14:54:14 +0100

    Binary addition

--- src/utils/encryption.py
+++ src/utils/encryption.py
@@ -15,3 +15,64 @@ def encrypt(password: str):
 
 def decrypt(password: str):
     return encrypt(password)
+
+def addition_binary(a, b):
+    string_a = str(a)[::-1]
+    string_b = str(b)[::-1]
+    is_retenu = False
+    
+    final = ""
+    
+    for i in range(len(string_a) if len(string_a) >= len(string_b) else len(string_b)):
+        to_a = string_a[i] if len(string_a) -1 >= i else "0"
+        to_b = string_b[i] if len(string_b) -1 >= i else "0"
+
+        print('_________IN___________')
+        print('i: ', i)
+        print('to_a: ', to_a)
+        print('to_b: ', to_b)
+        print('result: ', simple_add(to_a, to_b))
+        print('retenu: ', is_retenu)
+        print('final: ', final)
+
+        if len(final) > 0 and final[0] == "1" and to_a == "1" and to_b == "0":
+            is_retenu = False
+        
+        if is_retenu:
+            final = "1" + final
+            is_retenu = False
+
+
+        
+        else:
+            if to_a == to_b and to_a == "1":
+                final += "0"
+                is_retenu = True
+            else:
+                final = str(simple_add(to_a, to_b)) + final
+
+        print('_________OUT___________')
+        print('i: ', i)
+        print('to_a: ', to_a)
+        print('to_b: ', to_b)
+        print('result: ', simple_add(to_a, to_b))
+        print('retenu: ', is_retenu)
+        print('final: ', final)
+    print(is_retenu)
+    return final
+
+
+def simple_add(a, b):
+    if a == "0":
+        if b == "0":
+            return 0
+        else: return 1
+    else: 
+        if b == "0":
+            return 1
+        else: return 0
+
+print(addition_binary(1011, 1))
+
+
+
